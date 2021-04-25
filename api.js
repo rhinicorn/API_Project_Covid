ea67880d07821dd04d5782823e42e12c1b2f4fd4
Rhinicorn <admin@rhinicorn.com>
2021-04-24 23:41:02 -0400

    Final updates to html layout and css. The data manipulation and display is almost complete.

--- api.js
+++ api.js
@@ -1,50 +1,97 @@
 
 const baseURL = 'https://api.covid19api.com/summary'
 
-const searchForm = document.querySelector('form');
-const globalButton = document.querySelector('#globalSubmit')
+//globalButton.addEventListener('submitCountry', fetchData1);
+let globalResultsArr = []
+let globalLog = document.getElementById("global-log")
+const myGlobal = document.getElementById("submitGlobal")
 
-globalButton.addEventListener('submitGlobal', fetchData);
-searchForm.addEventListener('submitCountry', fetchData1);
+//countryCode.addEventListener('getCountry', fetchData)
+let countryResultsArr = []
+let countryLog = document.getElementById("country-log")
 
+let countryCode = document.getElementById("getCountry")
+let myCountry = document.getElementById("submitCountry")
 
-//GLOBAL
+//FETCH FOR GLOBAL
 function fetchData1(e){
-    e.preventDefault();
+    //e.preventDefault()
+
+    fetch(baseURL)
+        .then(result => result.json())
+        .then(json => {
+            let globalData = json.Global
+            displayGlobalResults(globalData);
+        })
+}
+
+//DISPLAY RESULTS FOR GLOBAL
+function displayGlobalResults(Data){
+    console.log(Data)
+    let myresults = {
+        tConfirmed: Data.TotalConfirmed,
+        tDeaths: Data.TotalDeaths,
+        tRecoveries: Data.TotalRecovered
+    }
+    globalResultsArr.push(myresults)
+    renderGlobalResults()
+}
+
+//CREATE THE TABLE RENDERING FOR GLOBAL
+function renderGlobalResults(){
+    globalLog.innerHTML = ""
+    for(let i = 0; i<globalResultsArr.length; i++){
+        let row = createTableRow(globalResultsArr[i])
+        globalLog.appendChild(row)
+    }
+}
+
+//ADD ROWS USING FETCHED DATA FOR GLOBAL
+//used base template structre from code-along example
+function createTableRow(e) {
+    console.log(e)
+    
+    let tableRow = document.createElement('tr')
+    let tcases = document.createElement('td')
+    let tdeaths = document.createElement('td')
+    let trecoveries = document.createElement('td')
+    
+    tcases.innerText = e.tConfirmed
+    tdeaths.innerText = e.tDeaths
+    trecoveries.innerText = e.tRecoveries
+
+    tableRow.appendChild(tcases)
+    tableRow.appendChild(tdeaths)
+    tableRow.appendChild(trecoveries)
+
+    return tableRow
+}
+
+
+//FETCH FOR SPECIFIC COUNTRY
+function fetchData(e){
+    //e.preventDefault();
 
     fetch(baseURL)
         .then(result => result.json())
         .then(json => {
             console.log(json);
             let countryData = json.Countries
-            let globalData = json.Global
-            displayResults1(countryData);
+            displayResults(countryData);
         })
 }
-
-//COUNTRY
-function fetchData(e){
-    e.preventDefault();
-
-    fetch(baseURL)
-        .then(result => result.json())
-        .then(json => {
-            //console.log(json);
-            let countryData = json.Countries
-            let globalData = json.Global
-            displayResults(globalData);
-        })
-}
-
-
-
+//DISPLAY RESULTS FOR COUNTRY
 function displayResults(countryData){
-    
     console.log(countryData)
-    //get the value from the text search box
-    let country1 = document.getElementById('getCountry1').value
-    console.log(country1)
+    let myresults = {
+        tConfirmed: countryData.TotalConfirmed,
+        tDeaths: countryData.TotalDeaths,
+        tRecoveries: countryData.TotalRecovered
+    }
 
+    //get the value from the text search box
+    let country1 = document.getElementById('getCountry').value
+    console.log(country1)
 
     for(let i=0;i<countryData.length;i++){
         //console.log(countryData[i].CountryCode)
@@ -52,14 +99,14 @@ function displayResults(countryData){
             document.getElementById("results").innerHTML = "Country: "+ countryData[i].Country +"\nTotal Confirmed Cases: "+ countryData[i].TotalConfirmed +"\n Total Deaths: "+countryData[i].TotalDeaths +"\n Total Recovered: "+countryData[i].TotalRecovered
         }
     }
-
-    let country2 = document.getElementById('getCountry2').value
-
-
-    //filter by country
-    //display data."whatever the field is"
-    
-
-
-        
+    countryResultsArr.push(myresults)
+    renderCountryResults()     
+}
+//CREATE THE TABLE RENDERING FOR COUNTRY
+function renderCountryResults(){
+    countryLog.innerHTML = ""
+    for(let i = 0; i<countryResultsArr.length; i++){
+        let row = createTableRow(countryResultsArr[i])
+        countryLog.appendChild(row)
+    }
 }
